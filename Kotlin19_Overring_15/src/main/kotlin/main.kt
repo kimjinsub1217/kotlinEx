fc1ada55c38fd3d45e5915eb33e36cace92b0e62
kimjinsub1217 <hikm9985@naver.com>
2023-05-15 19:57:08 +0900

    오버로딩 리팩토링 숙제 완료

--- Kotlin19_Overring_15/src/main/kotlin/main.kt
+++ Kotlin19_Overring_15/src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-import java.util.Scanner
+import java.util.*
 
 //중국집
 //
@@ -30,85 +30,89 @@ import java.util.Scanner
 //
 //프로젝트의 이름은 Kotlin15_InheritEX1 로 해주세요
 
-fun main(){
+fun main() {
     val chinessRestaurant = ChinessRestaurant()
     chinessRestaurant.selectMenu()
     chinessRestaurant.printMenu()
     chinessRestaurant.printFoodList()
 }
 
-class ChinessRestaurant{
+class ChinessRestaurant {
     val scanner = Scanner(System.`in`)
+
     // 음식들을 담을 리스트
     val jjanJangMyunList = ArrayList<JJaJangMyun>()
     val jjamPPongList = ArrayList<JJamPPong>()
     val bockUmBabList = ArrayList<BockUmBab>()
 
     // 메뉴를 선택하는 기능
-    fun selectMenu(){
+    fun selectMenu() {
         var selectNumber = 0
 
-        while (true){
+        while (true) {
             println("메뉴를 선택해주세요")
             print("1. 짜장면, 2. 짬뽕, 3. 볶음밥, 4. 종료 : ")
             selectNumber = scanner.nextInt()
-
-            if(selectNumber !in 1..4){
+            var food: Food? = null
+            if (selectNumber !in 1..4) {
                 println("번호를 다시 입력해주세요")
                 continue
             }
-            if(selectNumber == 4){
+            if (selectNumber == 4) {
                 break
             }
 
-            when(selectNumber){
+            when (selectNumber) {
                 1 -> {
                     print("곱배기 인가요 ?(1. 곱배기O, 2. 곱배기X) : ")
                     val temp = scanner.nextInt()
                     var temp2 = true
-                    if(temp == 2){
+                    if (temp == 2) {
                         temp2 = false
                     }
-                    val food = JJaJangMyun("짜장면", 6000, temp2)
+                    food = JJaJangMyun(temp2)
                     jjanJangMyunList.add(food)
                 }
+
                 2 -> {
                     print("홍합이 있나요? (1. 있음, 2. 없음) : ")
                     val temp = scanner.nextInt()
                     var temp2 = true
-                    if(temp == 2){
+                    if (temp == 2) {
                         temp2 = false
                     }
-                    val food = JJamPPong("짬뽕", 8000, temp2)
+                    food = JJamPPong(temp2)
                     jjamPPongList.add(food)
                 }
+
                 3 -> {
                     print("국물 종류가 무엇인가요? (1. 짬뽕국물, 2.계란국물) : ")
                     val temp = scanner.nextInt()
-                    var temp2 = "짬뽕국물"
-                    if(temp == 2){
-                        temp2 = "계란국물"
+                    var temp2 = true
+                    if (temp == 2) {
+                        temp2 = false
                     }
-                    val food = BockUmBab("볶음밥", 10000, temp2)
+                   food = BockUmBab(temp2)
                     bockUmBabList.add(food)
                 }
             }
         }
     }
+
     // 주문 내역을 출력하는 기능
-    fun printMenu(){
+    fun printMenu() {
         // 총 주문 금액
         var totalPrice = 0
         // 짜장면 금액 누적
-        for(food in jjanJangMyunList){
+        for (food in jjanJangMyunList) {
             totalPrice += food.price
         }
         // 짬뽕 금액 누적
-        for(food in jjamPPongList){
+        for (food in jjamPPongList) {
             totalPrice += food.price
         }
         // 볶음밥 금액 누적
-        for(food in bockUmBabList){
+        for (food in bockUmBabList) {
             totalPrice += food.price
         }
 
@@ -118,31 +122,32 @@ class ChinessRestaurant{
         println("볶음밥 : ${bockUmBabList.size}개")
         println()
     }
+
     // 주문한 음식들을 출력하는 기능
-    fun printFoodList(){
+    fun printFoodList() {
         // 짜장면
-        for(food in jjanJangMyunList){
+        for (food in jjanJangMyunList) {
             food.printFoodInfo()
         }
         // 짬뽕
-        for(food in jjamPPongList){
+        for (food in jjamPPongList) {
             food.printFoodInfo()
         }
         // 볶음밥
-        for(food in bockUmBabList){
+        for (food in bockUmBabList) {
             food.printFoodInfo()
         }
     }
 
 }
 
-open class JJaJangMyun  (var name:String, var price:Int, var isDouble:Boolean){
+open class Food(open var name: String, open var price: Int, open var isDouble: Boolean) {
 
-    open fun printFoodInfo(){
+    open fun printFoodInfo() {
         println("음식 : $name")
         println("가격 : ${price}원")
 
-        if(isDouble == true){
+        if (isDouble == true) {
             println("곱배기 여부 : 곱배기 입니다")
         } else {
             println("곱배기 여부 : 곱배기 아닙니다")
@@ -150,13 +155,35 @@ open class JJaJangMyun  (var name:String, var price:Int, var isDouble:Boolean){
     }
 }
 
-class JJamPPong (var name:String, var price:Int, var hasMuseel:Boolean){
+class JJaJangMyun() : Food("짜장면", 6000, true) {
 
-    fun printFoodInfo(){
+    constructor(isDouble2: Boolean) : this() {
+        this.isDouble = isDouble2
+    }
+
+    override fun printFoodInfo() {
         println("음식 : $name")
         println("가격 : ${price}원")
 
-        if(hasMuseel == true){
+        if (isDouble == true) {
+            println("곱배기 여부 : 곱배기 입니다")
+        } else {
+            println("곱배기 여부 : 곱배기 아닙니다")
+        }
+    }
+}
+
+class JJamPPong() : Food("짬뽕", 8000, true) {
+
+    constructor(isDouble2: Boolean) : this() {
+        this.isDouble = isDouble2
+    }
+
+    override fun printFoodInfo() {
+        println("음식 : $name")
+        println("가격 : ${price}원")
+
+        if (isDouble == true) {
             println("홍합 여부 : 홍합이 있습니다")
         } else {
             println("홍합 여부 : 홍합이 없습니다")
@@ -164,12 +191,21 @@ class JJamPPong (var name:String, var price:Int, var hasMuseel:Boolean){
     }
 }
 
-class BockUmBab (var name:String, var price:Int, var soupType:String){
+class BockUmBab() : Food("볶음밥", 10000, true) {
 
-    fun printFoodInfo(){
+    constructor(isDouble2: Boolean) : this() {
+        this.isDouble = isDouble2
+    }
+
+    override fun printFoodInfo() {
         println("음식 : $name")
         println("가격 : ${price}원")
-        println("국물 종류 : $soupType")
+
+        if (isDouble == true) {
+            println("국물 종류 : 짬뽕국물")
+        } else {
+            println("국물 종류 : 계란국물")
+        }
     }
 }
 
